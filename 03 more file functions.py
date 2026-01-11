f6078b7ff2183f3f3318dda4ddd7ade8f1532645
its-mohan79 <mohanroy8989@gmail.com>
2026-01-11 23:38:31 +0530

    complete the lesson 3

---
+++ 03 more file functions.py
@@ -0,0 +1,32 @@
+f = open("file.txt")
+#method 1
+lines = f.readlines()
+print(lines, type(lines))
+
+
+#method 2
+
+line1 = f.readline()
+print(line1, type(line1))
+
+line2 = f.readline()
+print(line2, type(line2))
+
+line3 = f.readline()
+print(line3, type(line3))
+
+line4 = f.readline()
+print(line4, type(line4))
+
+line5 = f.readline()
+print(line5, type(line5))
+
+#method 3
+
+line = f.readline()
+while(line != ""):
+    print(line)
+    line = f.readline()
+
+
+f.close()
